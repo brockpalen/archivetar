465f7f0ea6c2407af0b7e2b0659ae0ee9a562e8d
Brock E. Palen <brockp@umich.edu>
2020-07-03 21:14:53 -0400

    added test for SuperTar compression selection behavior

--- test/test_SuperTar.py
+++ test/test_SuperTar.py
@@ -12,7 +12,7 @@ def test_SuperTar():
     tar = SuperTar()
     pp(tar._flags)
 
-@pytest.mark.parametrize("kwargs,kresult,,kwresult,expex", [
+@pytest.mark.parametrize("kwargs,kresult,kwresult,expex", [
            ({'verbose': True}, ['tar', '--sparse', '--verbose'], {'check': True}, does_not_raise()),
            ({}, ['tar', '--sparse'], {'check': True}, does_not_raise())
            ])
@@ -27,3 +27,18 @@ def test_SuperTar_opts_addfromfile(monkeypatch, kwargs, kresult, kwresult, expex
     tar.addfromfile('/does/not/exist')
     kresult.append('--files-from=/does/not/exist')
     mock.assert_called_once_with(kresult, **kwresult)
+
+
+@pytest.mark.parametrize("kwargs,mreturn,expex", [
+           ({'compress': 'GZIP'}, False, BaseException),  # GZIP requested but none found
+           ({'compress': 'NOT REAL'}, '/usr/bin/gzip', BaseException),     # No real parser
+           ])
+def test_SuperTar_ops_comp(monkeypatch, kwargs, mreturn, expex):
+    """check how compressions handlers behave when not found or not exist"""
+
+    mock = Mock(spec=shutil)
+    mock.return_value=mreturn
+    monkeypatch.setattr(shutil, "which", mock)
+
+    with pytest.raises(expex):
+        tar = SuperTar(**kwargs)
