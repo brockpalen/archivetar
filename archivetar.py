2decbb6ef04891781bc82c5b8773a5148d4b0359
Brock Palen <brockp@umich.edu>
2020-07-07 16:55:58 -0400

    added distrubiton print out on first scan

--- archivetar.py
+++ archivetar.py
@@ -296,6 +296,7 @@ def build_list(path=False,
         inst='/home/brockp/mpifileutils/install',
         mpirun='/sw/arcts/centos7/stacks/gcc/8.2.0/openmpi/4.0.3/bin/mpirun',
         sort='name',
+        filter=['--distribution', 'size:0,1K,1M,10M,100M,1G,10G,100G,1T'],
         progress='10')
 
     # generate timestamp name
