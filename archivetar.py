ce4fd00992150ce99695e3d9e85185e890f80c1a
Brock Palen <brockp@umich.edu>
2020-07-07 23:33:10 -0400

    added flags for --tar-verbose --remove-files and compression flags

--- archivetar.py
+++ archivetar.py
@@ -23,8 +23,6 @@ import argparse
 import os
 import pathlib
 import re
-import shutil
-import subprocess
 import sys
 import logging
 import datetime
@@ -33,50 +31,7 @@ import tempfile
 import humanfriendly
 
 from mpiFileUtils import DWalk
-
-
-def find_gzip():
-    """find pigz if installed in PATH otherwise return gzip"""
-    pigz = shutil.which('pigz')
-    gzip = shutil.which('gzip')
-    if pigz:
-        return pigz
-    elif gzip:
-        return gzip
-    else:
-        raise Exception("gzip compression but no gzip or pigz found in PATH")
-
-
-def find_bzip():
-    """find pigz if installed in PATH otherwise return gzip"""
-    lbzip2 = shutil.which('lbzip2')
-    pbzip2 = shutil.which('pbzip2')
-    bzip2 = shutil.which('bzip2')
-    if lbzip2:
-        return lbzip2
-    elif pbzip2:
-        return pbzip2
-    elif bzip2:
-        return bzip2
-    else:
-        raise Exception("gzip compression but no gzip or pigz found in PATH")
-
-
-def find_xz():
-    """find pixz if installed in PATH otherwise return xz"""
-    pixz = shutil.which('pixz')
-    xz = shutil.which('xz')
-    if pixz:
-        return pixz
-    elif xz:
-        return xz
-    else:
-        raise Exception("lzma/xz compression but no pixz or xz found in PATH")
-
-
-def find_lzma():
-    """alias for find_xz()"""
-    return find_xz()
+from SuperTar import SuperTar
 
 
 class DwalkLine:
@@ -162,63 +117,6 @@ class DwalkParser:
         yield self.indexcount, index_p, tartmp_p
 
 
-class SuperTar:
-    """ tar wrapper class for high speed """
-    # requires gnu tar
-    def __init__(self,
-                 filename=False,  # path to file eg output.tar
-                 compress=False,  # compress or not False | GZIP | BZ2 | LZ4
-                 verbose=False,  # print extra information when arching
-                 purge=False):   # pass --remove-files
-
-        if not filename:   # filename needed  eg tar --file <filename>
-            raise Exception("no filename given for tar")
-
-        self.filename = filename
-
-        self._flags = ["tar", "--sparse", '--create']
-
-        self.compsuffix = None
-        if compress == 'GZIP':
-            self._flags.append(f'--use-compress-program={find_gzip()}')
-            self.compsuffix = '.gz'
-        elif compress == 'BZ2':
-            self._flags.append(f'--use-compress-program={find_bzip()}')
-            self.compsuffix = '.bz2'
-        elif compress == 'XZ':
-            self._flags.append(f'--use-compress-program={find_xz()}')
-            self.compsuffix = '.xz'
-        elif compress == 'LZ4':
-            self._flags.append('--lz4')
-            self.compsuffix = '.lz4'
-        elif compress:
-            raise Exception("Invalid Compressor {compress}")
-
-        if verbose:
-            self._flags.append('--verbose')
-            self._verbose = True
-
-        if purge:
-            self._flags.append('--remove-files')
-
-    def addfromfile(self, path):
-        """load list of files from file eg tar -cvf output.tar --files-from=<file>"""
-        self._flags.append(f'--files-from={path}')
-
-    def addfrompath(self, path):
-        """load from fs path eg tar -cvf output.tar /path/to/tar"""
-        pass
-
-    def invoke(self):
-        """"actually kick off the tar"""
-        if self.compsuffix:
-            self.filename = f"{self.filename}{self.compsuffix}"
-
-        self._flags += ['--file', self.filename]
-        logging.debug(f"Tar invoked with: {self._flags}")
-        subprocess.run(self._flags, check=True)
-
-
 #############  MAIN  ################
 
 def parse_args(args):
@@ -253,6 +151,9 @@ def parse_args(args):
     verbosity = parser.add_mutually_exclusive_group()
     verbosity.add_argument('-v', '--verbose',
                            help="Increase messages, including files as added",
+                           action="store_true")
+    verbosity.add_argument('--tar-verbose',
+                           help="Pass -v to tar (print files as tar'd)",
                            action="store_true")
     verbosity.add_argument('-q', '--quiet',
                            help="Decrease messages",
@@ -384,7 +285,23 @@ if __name__ == "__main__":
             if not args.dryrun:
                 # if compression
                 # if remove
-                tar = SuperTar(filename=f"{args.prefix}-{index}.tar", purge=True)
+                t_args = {'filename': f"{args.prefix}-{index}.tar"}
+                if args.remove_files:
+                    t_args['purge'] = True
+                if args.tar_verbose:
+                    t_args['verbose'] = True
+
+                # compression options
+                if args.gzip:
+                    t_args['compress'] = 'GZIP'
+                if args.bzip:
+                    t_args['compress'] = 'BZ2'
+                if args.lz4:
+                    t_args['compress'] = 'LZ4'
+                if args.xz:
+                    t_args['compress'] = 'XZ'
+
+                tar = SuperTar(**t_args)
                 tar.addfromfile(tar_list)
                 tar.invoke()
 
