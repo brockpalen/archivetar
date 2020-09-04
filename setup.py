170b62bd74d2702c4124a4831add1149bc7c1ef8
Brock Palen <brockp@umich.edu>
2020-09-04 00:07:26 -0400

    version bump for unarchivetar

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="archivetar",
-    version="0.0.1",
+    version="0.0.2",
     author="Brock Palen",
     author_email="brockp@umich.edu",
     description="Prep folder for archive",
